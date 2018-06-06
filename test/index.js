3bf0b3eaddc46dfe219561cfcc94fc436324c31a
뎁온누리 <devonnuri@gmail.com>
2018-06-06 23:23:28 +0900

    Use class instead of prototype

--- test/index.js
+++ test/index.js
@@ -1,4 +1,6 @@
+const assert = require('assert');
+
 const dateMe = require('../src');
 
 console.log(dateMe.now().toString());
-console.log(dateMe.from({ year: 2019 }));
+console.log(dateMe.from({ year: 2019 }).toString());
